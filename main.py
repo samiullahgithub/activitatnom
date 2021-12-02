3c5e06a279e9983d91491526084669401135715d
samiullah <cf18ullah.sami@iesjoandaustria.org>
2021-12-02 13:16:52 +0100

    nom

---
+++ main.py
@@ -0,0 +1,15 @@
+# This r classes, files, tool windows, actions, and settings.
+
+
+def main():
+    nomber=str(input("introduce un nombre"))
+    cognom =str(input("introduce un cognom"))
+    cognom2 = str(input("introduce un cognom2"))
+    print(nomber[0:2])
+    print(cognom[0:2])
+    print(cognom2[0:2])
+
+if __name__ == '__main__':
+    main()
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
